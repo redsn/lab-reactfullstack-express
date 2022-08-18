d6b2815680e29ccb93a84a3e1c77c16ce768137a
tkl <tluk92@gmail.com>
2022-08-17 23:01:32 -0400

    wip: added mongoose schema, added defaults for image. Started GET for index and Create

--- server.js
+++ server.js
@@ -3,10 +3,13 @@
 ////////////////////////////////
 // get .env variables
 require("dotenv").config();
+const cors = require('cors');
+const morgan = require('morgan');
 
 // pull PORT from .env, give default value of 4000
 const { PORT = 4000, DATABASE_URL} = process.env;
 
+const { urlencoded } = require("express");
 // import express, mongoose
 const express = require("express");
 const mongoose = require('mongoose');
@@ -24,6 +27,34 @@ db.on('error', (err)=> {console.log('an error occured' + err.message)});
 // create application object
 const app = express();
 
+/// Body parse middleware
+// urlencoded()
+// json() 
+// Both will 'use' req.body
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+
+/////////////
+/////////////////////////////////////////////////////////////
+///Models////
+/////////////
+
+const Schema = mongoose.Schema;
+
+const peopleSchema = new Schema({
+    name: String,
+    image: {type: String, default:"https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"},
+    title: String
+}, {timestamps: true, });
+
+const People = mongoose.model('People',  peopleSchema)
+
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
 ///////////////////////////////
 // ROUTES
 ////////////////////////////////
@@ -31,6 +62,33 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("hello world");
 });
+
+//// CONTROLLERS ////
+////INDEX///////////
+// app.get('/people', (req,res) => {
+//     People.find({}, (err, index) => {
+
+//     })
+// });
+app.get('/people', async (req,res)=>{
+    // const people = await People.find({});
+    // res.send(people);
+    ////
+    try{
+    res.status(200).json(await People.find({}));
+    } catch (error) {
+        res.status(400).json({message: 'Bad Request'});
+    }
+});
+
+///CREATE/////
+app.post('/people', async (req,res) => {
+    try{
+        res.status(201).json(await People.create(req.body));
+    } catch(error){
+        res.status(400).json({message: 'Bad Request'});
+    }
+})
 
 ///////////////////////////////
 // LISTENER
